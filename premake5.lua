0fbd679587b675fd207984c816c8540913b6fd13
matty <mattreed1890@gmail.com>
2025-03-12 13:55:49 -0500

    update premake

--- premake5.lua
+++ premake5.lua
@@ -1,28 +1,28 @@
-workspace "OpenGLGame"
-    configurations { "Debug", "Release" }
-    platforms { "Win64" }
+workspace("OpenGLGame")
+configurations({ "Debug", "Release" })
+platforms({ "Win64" })
 
-project "OpenGLGame"
-    kind "ConsoleApp"
-    language "C++"
-    targetdir "bin/%{cfg.buildcfg}"
-    cppdialect "C++23"
+project("OpenGLGame")
+kind("ConsoleApp")
+language("C++")
+targetdir("bin/%{cfg.buildcfg}")
+cppdialect("C++23")
 
-    files { "include/**.h", "include/**.hpp", "src/**.c", "src/**.cpp", "shaders/**.vert", "shaders/**.frag" }
+files({ "include/**.h", "include/**.hpp", "src/**.c", "src/**.cpp", "shaders/**.vert", "shaders/**.frag" })
 
-    includedirs { "include" }
-    includedirs { "src" }
-    includedirs { "shaders" }
-    libdirs { "lib" }
+includedirs({ "include" })
+includedirs({ "src" })
+includedirs({ "shaders" })
+libdirs({ "lib" })
 
-    links { "glfw3" }
-    links { "OpenGL32" }
-    links { "assimp-vc143-mt" }
+links({ "glfw3" })
+links({ "OpenGL" })
 
-    filter "configurations:Debug"
-    defines { "DEBUG" }
-    symbols "On"
+filter("configurations:Debug")
+defines({ "DEBUG" })
+symbols("On")
 
-    filter "configurations:Release"
-    defines { "NDEBUG" }
-    optimize "On"
+filter("configurations:Release")
+defines({ "NDEBUG" })
+optimize("On")
+
